02e4a48df11bebb62fa03aec8c1d385aa08b3b6d
Sascha <sascha.dettloff@gmail.com>
2025-02-14 09:01:08 +0100

    Initial commit

--- api/get-time.js
+++ api/get-time.js
@@ -1,15 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 
-module.exports = async (req, res) => {
-    if (req.method === 'GET') {
-        try {
-            const data = fs.readFileSync(path.join(__dirname, '../timeEntries.json'), 'utf8');
-            res.status(200).json(JSON.parse(data));
-        } catch (error) {
-            res.status(500).send('Fehler beim Laden der Datei');
-        }
-    } else {
-        res.status(405).send('Method Not Allowed');
+module.exports = (req, res) => {
+    const dataFilePath = path.resolve('data/timeEntries.json');
+    try {
+        const fileData = fs.readFileSync(dataFilePath, 'utf8');
+        const timeEntries = JSON.parse(fileData);
+        res.status(200).json(timeEntries);
+    } catch (err) {
+        console.error('Fehler beim Laden der Datei:', err);
+        res.status(500).json({ error: 'Fehler beim Laden der Daten' });
     }
 };
--- api/save-time.js
+++ api/save-time.js
@@ -3,19 +3,28 @@ const path = require('path');
 
 module.exports = async (req, res) => {
     if (req.method === 'POST') {
-        const timeEntry = req.body;
+        const { date, startTime, endTime, breakDuration, totalHoursDecimal, totalHours, remainingMinutes } = req.body;
 
+        // Lade die vorhandenen Daten aus der JSON-Datei
+        const dataFilePath = path.resolve('data/timeEntries.json');
+        let timeEntries = [];
         try {
-            const data = fs.readFileSync(path.join(__dirname, '../timeEntries.json'), 'utf8');
-            let timeEntries = JSON.parse(data);
-            timeEntries.push(timeEntry);
-
-            fs.writeFileSync(path.join(__dirname, '../timeEntries.json'), JSON.stringify(timeEntries, null, 2));
-            res.status(200).send('Zeiterfassungsdaten erfolgreich gespeichert');
-        } catch (error) {
-            res.status(500).send('Fehler beim Speichern der Datei');
+            const fileData = fs.readFileSync(dataFilePath, 'utf8');
+            timeEntries = JSON.parse(fileData);
+        } catch (err) {
+            console.error('Fehler beim Laden der Datei:', err);
         }
+
+        // Füge den neuen Eintrag hinzu
+        const newEntry = { date, startTime, endTime, breakDuration, totalHoursDecimal, totalHours, remainingMinutes };
+        timeEntries.push(newEntry);
+
+        // Speichere die Daten zurück in die JSON-Datei
+        fs.writeFileSync(dataFilePath, JSON.stringify(timeEntries, null, 2));
+
+        // Sende eine Erfolgsmeldung zurück
+        res.status(200).json({ message: 'Zeiterfassungsdaten gespeichert' });
     } else {
-        res.status(405).send('Method Not Allowed');
+        res.status(405).json({ error: 'Method Not Allowed' });
     }
 };
