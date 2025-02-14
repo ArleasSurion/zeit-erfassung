358452947c69bb3050c07a65759d19d072058030
Sascha <sascha.dettloff@gmail.com>
2025-02-14 05:04:00 +0100

    Initial commit

--- public/script.js
+++ public/script.js
@@ -57,7 +57,13 @@ function saveTimeEntry(date, startTime, endTime, breakDuration, totalHoursDecima
 
 function displayTimeEntries() {
     fetch('/get-time')
-        .then(response => response.json())
+        .then(response => {
+            // Überprüfe, ob die Antwort erfolgreich war
+            if (!response.ok) {
+                throw new Error('Fehler beim Abrufen der Daten: ' + response.statusText);
+            }
+            return response.json();
+        })
         .then(data => {
             const timeEntriesList = document.getElementById("time-entries-list");
             let html = "<ul>";
@@ -67,7 +73,9 @@ function displayTimeEntries() {
             html += "</ul>";
             timeEntriesList.innerHTML = html;
         })
-        .catch(error => console.error('Fehler beim Laden der Daten:', error));
+        .catch(error => {
+            console.error('Fehler beim Laden der Daten:', error);
+            // Hier könntest du eine Fehlermeldung im UI anzeigen
+        });
 }
 
-document.addEventListener("DOMContentLoaded", displayTimeEntries);
